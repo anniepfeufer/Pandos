916f356b886736b4c77be5155e858089da0d452d
anniepfeufer <pfeufe_a1@denison.edu>
2025-02-03 16:00:45 -0500

    adding tree maintenence

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -55,3 +55,65 @@ void freePcb(pcb_t *p)
     p->p_next = pcbFree_h; // Insert PCB at the front of the free list
     pcbFree_h = p;
 }
+
+
+/**
+ *  Return TRUE if the pcb pointed to by p has no children.
+ *  Return FALSE otherwise. 
+ */
+int emptyChild(pcb_t *p)
+{
+    if (p->p_child == NULL)
+        return TRUE;
+
+    return FALSE;
+}
+
+/**
+ * Make the pcb pointed to by p a child of the pcb pointed to by prnt.
+ */
+void insertChild(pcb_t *prnt, pcb_t *p)
+{
+    pcb_t *first_sib = prnt->p_child;
+    prnt->p_child = p;
+    p->p_sib_right = first_sib;
+    first_sib->p_sib_left = p;
+    p->p_prnt = prnt;
+}
+
+/**
+ *  Make the first child of the pcb pointed to by p no longer a child of
+ * p. Return NULL if initially there were no children of p. Otherwise,
+ * return a pointer to this removed first child pcb. 
+ */
+pcb_t *removeChild(pcb_t *p)
+{
+    if (emptyChild(p))
+        return NULL;
+    pcb_t *removed = p->p_child;
+    return outChild(removed);
+}
+
+/**
+ *  Make the pcb pointed to by p no longer the child of its parent. If
+ * the pcb pointed to by p has no parent, return NULL; otherwise, return
+ * p. Note that the element pointed to by p need not be the first child of
+ * its parent. 
+ */
+pcb_t *outChild(pcb_t *p)
+{
+    if (p->p_prnt==NULL)
+        return NULL;
+
+    if (p->p_prnt->p_child == p)
+        p->p_prnt->p_child = p->p_sib_right;
+
+    p->p_sib_left->p_sib_right = p->p_sib_right;
+    p->p_sib_right->p_sib_left = p->p_sib_left;
+
+    p->p_prnt = NULL;
+    p->p_sib_left = NULL;
+    p->p_sib_right = NULL;
+
+    
+}
